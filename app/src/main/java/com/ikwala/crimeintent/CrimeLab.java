4971f13e3f900854a8739b06d255528d1a89e5c4
ikwala <ikwala@me.com>
2017-02-06 17:31:08 +0400

    for today Class

---
+++ app/src/main/java/com/ikwala/crimeintent/CrimeLab.java
@@ -0,0 +1,48 @@
+package com.ikwala.crimeintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by ikwala on 2/6/17.
+ */
+public class CrimeLab
+{
+    private static CrimeLab crimeLab;
+
+    private List<Crime> crimeList;
+
+
+    private CrimeLab(Context context)
+    {
+        crimeList = new ArrayList<>();
+        for (int i = 0; i < 100; i++)
+        {
+            Crime crime = new Crime();
+            crime.setTitle("Crime # " + i);
+            crime.setSolved(i % 2 == 0);
+            crimeList.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimeList()
+    {
+        return crimeList;
+    }
+
+    public Crime getCrime(UUID id)
+    {
+        for (Crime crime : crimeList)
+        {
+            if (crime.getId().equals(id))
+            {
+                return crime;
+            }
+        }
+
+        return null;
+    }
+}
